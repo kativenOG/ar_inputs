58b6b21ac62f06aaa47a197f5fdc144cfabcd668
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-06-22 11:06:24 +0200

    Fixed dupes

--- checker.py
+++ checker.py
@@ -1,9 +1,8 @@
 from pysmt.smtlib.parser import SmtLibParser
+import sys 
 
-for i in range(1,19):
-        parser = SmtLibParser()
-        filename = "input" + str(i) + ".smt2"
-        print(filename)
-        script = parser.get_script_fname(filename)
-        formula = script.get_strict_formula().serialize()
-        print(f"-{filename}:\n{formula}\n")
+parser = SmtLibParser()
+filename = "input" + sys.argv[1] + ".smt2"
+script = parser.get_script_fname(filename)
+formula = script.get_strict_formula().serialize()
+print(f"-{filename}: {formula}")
