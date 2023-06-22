4fa4b00d07b7a286d0402e4a23a7825bbc1242a6
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-06-22 14:11:35 +0200

    removed checker.py

--- checker.py
+++
@@ -1,8 +0,0 @@
-from pysmt.smtlib.parser import SmtLibParser
-import sys 
-
-parser = SmtLibParser()
-filename = "input" + sys.argv[1] + ".smt2"
-script = parser.get_script_fname(filename)
-formula = script.get_strict_formula().serialize()
-print(f"-{filename}: {formula}")
